c9b2d13218db97fea8b46410d499a6f3f57d2633
Matteo Tosato <matteo.tosato7@gmail.com>
2024-11-17 19:10:18 +0100

    update pub script

--- pub_monads_nuget.gihub.sh
+++ pub_monads_nuget.gihub.sh
@@ -8,7 +8,7 @@ fi
 rm -r ./nupkg/monads.*
 dotnet pack ./Monads/monads.csproj -o ./nupkg &&
 
-dotnet nuget push ./nupkg/*.nupkg \
+dotnet nuget push ./nupkg/monads.*.nupkg \
     --api-key $GITHUB_ACCESS_TOKEN \
     --source https://nuget.pkg.github.com/sandhaka/index.json \
     --skip-duplicate
